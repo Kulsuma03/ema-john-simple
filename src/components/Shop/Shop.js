6c6fc88fff5d5f43008c3f8236d7cca7d9528e3e
Kulsuma Akter <kulsumams72@gmail.com>
2022-09-26 14:41:21 +0600

    cart added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,21 +1,52 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useState([]);
+    // console.log(setCart)
     useEffect( () =>{
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     } ,[]);
 
-    const handleAddToCart= (product) =>{
-        console.log(product);
-        const newCart = [...cart, product];
+    useEffect( () => {
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        for(const id in storedCart){
+            const addedPoduct = products.find(product =>  product.id === id);
+            if(addedPoduct){
+                
+                const quantity = storedCart[id];
+                addedPoduct.quantity = quantity;
+                savedCart.push(addedPoduct);
+            }
+
+        }
+        setCart(savedCart) 
+       
+    },[products])
+
+    const handleAddToCart= (selectedProduct) =>{
+        console.log(selectedProduct);
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id)
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct]
+        }else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists]
+        }
+        
         setCart(newCart)
+        addToDb(selectedProduct.id)
+
   }
     return (
         <div className='shop-container'>
@@ -25,11 +56,12 @@ const Shop = () => {
                     products.map(product => <Product 
                         key=  {product.id}
                         product= {product}
-                        handler= {handleAddToCart} />)
+                        handler= {handleAddToCart}
+                        />)
                 }
             </div>
             <div className="cart-container">
-                <Cart cart= {cart.length}></Cart>
+                <Cart cart= {cart}></Cart>
                 
                 
             </div>
